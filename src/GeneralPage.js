070409d130c8b848eb41569939176e8e68d01171
Iuliia <mbeq9665>
2017-04-24 23:09:40 +0400

    Удаление лишнего

--- src/GeneralPage.js
+++ src/GeneralPage.js
@@ -9,11 +9,6 @@ class GeneralPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            message: "",
-            isFil1: false,
-            isFil2: false,
-            isFil3: false,
-            isFil4: false,
             isFilling1: false,
             isFilling2: false,
             isFilling3: false,
@@ -26,24 +21,24 @@ class GeneralPage extends Component {
     }
 
     onChange1 = (e) => {
-        this.setState({isFil1: e.target.value !== "", isFilling1: e.target.value !== "" });
+        this.setState({isFilling1: e.target.value !== ""});
     }
 
     onChange2 = (e) => {
-        this.setState({isFil2: e.target.value !== "", isFilling2: e.target.value !== "" });
+        this.setState({isFilling2: e.target.value !== ""});
     }
 
     onChange3 = (e) => {
-        this.setState({isFil3: e.target.value !== "", isFilling3: e.target.value !== ""  });
+        this.setState({isFilling3: e.target.value !== ""});
     }
 
     onChange4 = (e) => {
-        this.setState({isFil4: e.target.value !== "", isFilling4: e.target.value !== ""  });
+        this.setState({isFilling4: e.target.value !== ""});
     }
 
     render() {
         let isRequired = true;
-        let isDisabled = !(this.state.isFil1 && this.state.isFil2 && this.state.isFil3 && this.state.isFil4);
+        let isDisabled = !(this.state.isFilling1 && this.state.isFilling2 && this.state.isFilling3 && this.state.isFilling4);
         return (
             <div className="App">
                 <Field isRequired={isRequired} isFilling={this.state.isFilling1} onChange={this.onChange1}/> <br/>
